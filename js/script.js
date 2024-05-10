c07f6e613f6f8b89159fe709232801378898cdd9
Pavel <vladzimirskiy9@gmail.com>
2024-05-10 18:12:30 +0900

    test commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict"
+const obj = {
+	name: "John",
+	age: 25,
+	isMaried: false
+};
+console.log(obj);
+//массив
+let arr = ['plumm.png', 'orange.jpg'];
+//вызов по порядку с нуля
+console.log(arr);
+//alert('Hello'); 
+//let result = confirm('lol');
+//console.log(result);
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer);
+// const answers = [];
+// answers[0] = prompt("Imya", "");
+// answers[1] = prompt("Pol", "");
+// answers[2] = prompt("Rabota", "");
+// document.write(answers);
+let incr = 10,
+	decr = 10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+console.log(2*4 == 8)
+
+const isA = true,
+	isB = true;
+console.log(isA || !isB);
